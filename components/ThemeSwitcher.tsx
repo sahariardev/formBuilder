85fd28074510cee479718a126b3ccd6ec12d7bd9
sahariardev <rifatsahariar@gmail.com>
2024-02-04 21:46:28 +0600

    Completed theme switcher button

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -3,7 +3,7 @@
 import {useTheme} from "next-themes";
 import {useEffect, useState} from "react";
 import {Tabs, TabsList, TabsTrigger} from "@/components/ui/tabs";
-import {SunIcon} from "@radix-ui/react-icons";
+import {DesktopIcon, MoonIcon, SunIcon} from "@radix-ui/react-icons";
 
 function ThemeSwitcher() {
     const {theme, setTheme} = useTheme();
@@ -17,11 +17,19 @@ function ThemeSwitcher() {
     return (
         <div>
             <Tabs>
-              <TabsList className="border">
-                  <TabsTrigger value="light" onClick={() => setTheme("light")}>
-                     <SunIcon className="h-[1.2rem] w-[1.2rem]"/>
-                  </TabsTrigger>
-              </TabsList>
+                <TabsList className="border">
+                    <TabsTrigger value="light" onClick={() => setTheme("light")}>
+                        <SunIcon className="h-[1.2rem] w-[1.2rem]"/>
+                    </TabsTrigger>
+
+                    <TabsTrigger value="dark" onClick={() => setTheme("dark")}>
+                        <MoonIcon className="h-[1.2rem] w-[1.2rem] rotate-90 transition-all dark:rotate-0"/>
+                    </TabsTrigger>
+
+                    <TabsTrigger value="system" onClick={() => setTheme("system")}>
+                        <DesktopIcon className="h-[1.2rem] w-[1.2rem]"/>
+                    </TabsTrigger>
+                </TabsList>
             </Tabs>
         </div>
     );
