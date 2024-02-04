880572a4914a03001a646f0b8f7700435d93fdaa
Sahariar Alam Khandoker <sahariar.alam@therapservices.net>
2024-02-04 21:40:06 +0600

    Added navbar

---
+++ components/ThemeSwitcher.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import {useTheme} from "next-themes";
+import {useEffect, useState} from "react";
+import {Tabs, TabsList, TabsTrigger} from "@/components/ui/tabs";
+import {SunIcon} from "@radix-ui/react-icons";
+
+function ThemeSwitcher() {
+    const {theme, setTheme} = useTheme();
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) return null;
+    return (
+        <div>
+            <Tabs>
+              <TabsList className="border">
+                  <TabsTrigger value="light" onClick={() => setTheme("light")}>
+                     <SunIcon className="h-[1.2rem] w-[1.2rem]"/>
+                  </TabsTrigger>
+              </TabsList>
+            </Tabs>
+        </div>
+    );
+}
+
+export default ThemeSwitcher;
