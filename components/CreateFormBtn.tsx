8b75581fd2089959fbe62e79abba96f4349814e3
sahariardev <rifatsahariar@gmail.com>
2024-02-17 07:39:11 +0600

    Added create form form

--- components/CreateFormBtn.tsx
+++ components/CreateFormBtn.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import {
     Dialog,
     DialogContent,
-    DialogDescription,
+    DialogDescription, DialogFooter,
     DialogHeader,
     DialogTitle,
     DialogTrigger
@@ -15,6 +15,9 @@ import {zodResolver} from "@hookform/resolvers/zod";
 import * as z from "zod";
 import {useForm} from "react-hook-form";
 import {Input} from "@/components/ui/input";
+import {Textarea} from "@/components/ui/textarea";
+import {ImSpinner2} from "react-icons/im";
+import {toast} from "@/components/ui/use-toast";
 
 const formSchema = z.object({
     name: z.string().min(4),
@@ -30,7 +33,15 @@ function CreateFormBtn() {
     });
 
     function onSubmit(values: formSchemaType) {
-        console.log(values);
+        try {
+
+        } catch (error) {
+            toast({
+                title: "Error",
+                description: "Something went wrong, pleae try again later",
+                variant: "destructive"
+            });
+        }
     }
 
     return <Dialog>
@@ -60,8 +71,32 @@ function CreateFormBtn() {
                             </FormItem>
                         )}
                     />
+                    <FormField
+                        control={form.control}
+                        name="description"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Description</FormLabel>
+                                <FormControl>
+                                    <Textarea rows={5} {...field}/>
+                                </FormControl>
+                            </FormItem>
+                        )}
+                    />
                 </form>
             </Form>
+            <DialogFooter>
+                <Button
+                    onClick={() => {form.handleSubmit((onSubmit))}}
+
+                    disabled={form.formState.isSubmitting}
+                        className="w-full mt-4">
+
+                    {!form.formState.isSubmitting && <span>Save</span>}
+                    {form.formState.isSubmitting && <ImSpinner2 className="animate-spin"/>}
+
+                </Button>
+            </DialogFooter>
         </DialogContent>
     </Dialog>
 }
