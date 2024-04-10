354430375a8af1bfe5983f1c1234df11cf31ebb2
sahariardev <rifatsahariar@gmail.com>
2024-04-10 10:40:14 +0600

    Added form published view

--- components/FormBuilder.tsx
+++ components/FormBuilder.tsx
@@ -8,6 +8,11 @@ import Designer from "@/components/Designer";
 import {DndContext, MouseSensor, TouchSensor, useSensor, useSensors} from "@dnd-kit/core";
 import DragOverlayWrapper from "@/components/DragOverlayWrapper";
 import useDesigner from "@/components/hooks/useDesigner";
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+import {toast} from "@/components/ui/use-toast";
+import Link from "next/link";
+import {BsArrowLeft, BsArrowRight} from "react-icons/bs";
 
 function FormBuilder({form}: { form: Form }) {
 
@@ -32,6 +37,54 @@ function FormBuilder({form}: { form: Form }) {
         const elements = JSON.parse(form.content);
         setElements(elements);
     }, [form, setElements]);
+
+    const shareUrl = `${window.location, origin}/submit/${form.shareURL}`;
+
+    if (form.published) {
+        return (
+            <>
+                <div className="flex flex-col items-center justify-center h-full w-full">
+                    <div className="max-w-md">
+                        <h1 className="text-center text-4xl font-bold text-primary border-b pb-2 mb-10">
+                            Form Published
+                        </h1>
+                        <h2 className="text-2xl">
+                            Share this form
+                        </h2>
+                        <h3 className="text-xl text-muted-foreground border-b pb-10">
+                            Anyone with the link can view and submit the form
+                        </h3>
+                        <div className="my-4 flex flex-col gap-2 items-center w-full border-b pb-4">
+                            <Input className="w-full" readOnly value={shareUrl}/>
+                            <Button className="mt-2 w-full" onClick={() => {
+                                navigator.clipboard.writeText(shareUrl);
+                                toast({
+                                    title: "Copied!",
+                                    description: "Link copied to clipboard"
+                                });
+                            }}/>
+                        </div>
+                        <div className="flex justify-between">
+                            <Button variant={"link"} asChild>
+                                <Link href={"/"} className="gap-2">
+                                    <BsArrowLeft/>
+                                    Go back home
+                                </Link>
+                            </Button>
+                        </div>
+                        <div className="flex justify-between">
+                            <Button variant={"link"} asChild>
+                                <Link href={`/forms/${form.id}`} className="gap-2">
+                                    Form details
+                                    <BsArrowRight/>
+                                </Link>
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    }
 
     return (
         <DndContext sensors={sensors}>
