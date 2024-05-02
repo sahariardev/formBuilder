8f1dc2b50e6ed9492d49d6e46132e9f435784c21
sahariardev <rifatsahariar@gmail.com>
2024-05-02 19:10:46 +0600

    Updated sidebar

--- components/FormElementsSidebar.tsx
+++ components/FormElementsSidebar.tsx
@@ -10,9 +10,17 @@ function FormElementsSidebar() {
             <p className="text-sm text-foreground/70">Drag and drop elements</p>
             <Separator/>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-2 place-items-center">
-                <p className="text-sm text-muted-foreground col-span-1 md:col-span-2 my-2 place-self-start"></p>
-                <SidebarBtnElement formElement={FormElements.TextField}/>
+                <p className="text-sm text-muted-foreground col-span-1 md:col-span-2 my-2 place-self-start">
+                    Layout Elements
+                </p>
                 <SidebarBtnElement formElement={FormElements.TitleField}/>
+                <SidebarBtnElement formElement={FormElements.SubtitleField}/>
+
+                <p className="text-sm text-muted-foreground col-span-1 md:col-span-2 my-2 place-self-start">
+                    Form Elements
+                </p>
+
+                <SidebarBtnElement formElement={FormElements.TextField}/>
             </div>
         </div>
     );
